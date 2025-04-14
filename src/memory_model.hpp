a8df842ec0cb9bf4bec4906c458b8cfd1c5aeab1
esterlein <esterlein.git@gmail.com>
2025-04-14 14:35:56 -0400

    new memory model

--- src/memory_model.hpp
+++ src/memory_model.hpp
@@ -1,9 +1,10 @@
 #pragma once
 
 #include "metapool.hpp"
+#include "metapool_config.hpp"
+#include "metapool_registry.hpp"
 #include "allocator.hpp"
 #include "monotonic_arena.hpp"
-#include "metapool_registry.hpp"
 
 
 namespace hpr {
@@ -11,10 +12,10 @@ namespace mem {
 
 
 	static inline constexpr std::size_t arena_size = 268435456; // 256 MB
-
+	
 
 	using StandardMetapoolRegistry =
-		MetapoolRegistry <
+		MetapoolRegistry
 			Metapool<mem::MetapoolConfig<1024, 8, 16, 32, 64, 128, 256>>
 		>;
 
@@ -39,12 +40,6 @@ public:
 		if constexpr (Type == mem::AllocatorType::Standard) {
 			return create_thread_local_allocator<mem::StandardMetapoolRegistry>();
 		}
-		else if constexpr (Type == mem::AllocatorType::Standard) {
-			return create_thread_local_allocator<mem::StandardMetapoolRegistry>();
-		}
-		else if constexpr (Type == mem::AllocatorType::Standard) {
-			return create_thread_local_allocator<mem::StandardMetapoolRegistry>();
-		}
 	}
 
 private:
@@ -53,7 +48,6 @@ private:
 	static auto create_metapools()
 	{
 		using MetapoolsTuple = typename MetapoolRegistry::tuple_type;
-
 		thread_local static MonotonicArena arena{mem::arena_size, mem::cacheline};
 
 		return []<std::size_t... Is>(std::index_sequence<Is...>) {
@@ -64,13 +58,13 @@ private:
 	}
 
 	template <typename MetapoolsTuple>
-	static auto create_descriptors(MetapoolsTuple& metapools)
+	static auto create_proxies(MetapoolsTuple& metapools)
 	{
 		return [&metapools]<std::size_t... Is>(std::index_sequence<Is...>) {
-			return std::array<MetapoolDescriptor, sizeof...(Is)> {
-				([&metapools]() -> MetapoolDescriptor {
+			return std::array<MetapoolProxy, sizeof...(Is)> {
+				([&metapools]() -> MetapoolProxy {
 					auto& pool = std::get<Is>(metapools);
-					return pool.descriptor();
+					return pool.proxy();
 				}())...
 			};
 		}(std::make_index_sequence<std::tuple_size_v<std::decay_t<decltype(metapools)>>>{});
@@ -80,22 +74,11 @@ private:
 	static auto& create_thread_local_allocator()
 	{
 		thread_local static auto metapools = create_metapools<MetapoolRegistryType>();
-
-		thread_local static auto descriptors = create_descriptors(metapools);
-
-		using MetapoolDescriptorArray = std::decay_t<decltype(descriptors)>;
-		using AllocatorConfigType = mem::AllocatorConfig<MetapoolDescriptorArray>;
-
-		thread_local static constexpr AllocatorConfigType config {
-			.alignment_quantum = mem::alignment_quantum,
-			.alignment_shift   = MetapoolBase::alloc_header_size,
-			.min_stride_step   = mem::min_stride_step,
-			.min_stride        = MetapoolRegistryType::min_stride,
-			.max_stride        = MetapoolRegistryType::max_stride
-		};
-
-		thread_local static Allocator<AllocatorConfigType> allocator(descriptors);
-
+		thread_local static auto proxies = create_proxies(metapools);
+		
+		constexpr auto allocator_config = MetapoolRegistryType::create_allocator_config();
+		
+		thread_local static Allocator<decltype(allocator_config)> allocator(proxies);
 		return allocator;
 	}
 };
