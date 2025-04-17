ad7e3679c2e4156eb1f88c6236c57ccd547494dd
esterlein <esterlein.git@gmail.com>
2025-04-17 13:48:34 -0400

    packed alloc header

--- src/alloc_header.hpp
+++ src/alloc_header.hpp
@@ -6,10 +6,27 @@
 namespace hpr {
 namespace mem {
 
+
 		struct AllocHeader
 		{
-			uint8_t pool_index;
-			uint8_t magic = 0xAB;
+			uint16_t encoded_index;
+
+			static constexpr AllocHeader make(uint8_t mpool, uint8_t flist)
+			{
+				return AllocHeader {
+					static_cast<uint16_t>((mpool << 8) | flist)
+				};
+			}
+
+			uint8_t pool_index() const
+			{
+				return static_cast<uint8_t>(encoded_index >> 8);
+			}
+
+			uint8_t flist_index() const
+			{
+				return static_cast<uint8_t>(encoded_index & 0xFF);
+			}
 		};
 
 		static constexpr auto alloc_header_size = sizeof(AllocHeader);
