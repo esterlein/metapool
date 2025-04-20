bca820281ef052ecacbce6104daa7e102efe4b3a
esterlein <esterlein.git@gmail.com>
2025-04-20 16:56:41 -0400

    update alloc config

--- src/allocator_config.hpp
+++ src/allocator_config.hpp
@@ -39,52 +39,59 @@ namespace mem {
 	};
 
 
-	template <typename T>
-	concept IsAllocatorConfig = requires {
-		typename T::tag;
-		typename T::proxy_array_type;
+template <typename T>
+concept IsAllocatorConfig = requires {
 
-		std::same_as<typename T::tag, allocator_config_tag>;
+	typename T::tag;
+	typename T::ProxyArrayType;
 
-		T::range_metadata;
+	{ T::range_metadata } -> std::same_as<decltype(T::range_metadata)>;
+	{ T::range_count }    -> std::convertible_to<std::size_t>;
 
-	} && []() constexpr {
-		const auto& ranges = T::range_metadata;
+} && []() constexpr {
 
-		if (ranges.size() == 0) {
+	const auto& ranges      = T::range_metadata;
+	const std::size_t range_count = ranges.size();
+
+	if (range_count == 0) {
+		return false;
+	}
+
+	for (std::size_t i = 0; i < range_count; ++i) {
+		const auto& this_range = ranges[i];
+
+		if (this_range.stride_step == 0) {
 			return false;
 		}
 
-		for (std::size_t i = 0; i < ranges.size(); ++i) {
-			const auto& range = ranges[i];
-
-			if (range.stride_step == 0) {
+		if ((this_range.stride_step & (this_range.stride_step - 1)) == 0) {
+			uint32_t mask = this_range.stride_step - 1;
+			if ((this_range.stride_min & mask) ||
+				(this_range.stride_max & mask))
+			{
 				return false;
 			}
-
-			if ((range.stride_step & (range.stride_step - 1)) == 0) {
-				const uint32_t mask = range.stride_step - 1;
-				if ((range.stride_min & mask) != 0 || (range.stride_max & mask) != 0) {
-					return false;
-				}
-			}
-			else {
-				if (range.stride_min % range.stride_step != 0 || 
-					range.stride_max % range.stride_step != 0) {
-					return false;
-				}
-			}
-
-			if (i < ranges.size() - 1) {
-				const auto& next = ranges[i + 1];
-				if (range.stride_max + range.stride_step != next.stride_min) {
-					return false;
-				}
+		}
+		else {
+			if ((this_range.stride_min % this_range.stride_step) != 0 ||
+				(this_range.stride_max % this_range.stride_step) != 0)
+			{
+				return false;
 			}
 		}
 
-		return true;
-	}();
+		if (i + 1 < range_count) {
+			const auto& next_range = ranges[i + 1];
+			if (this_range.stride_max + this_range.stride_step
+				!= next_range.stride_min)
+			{
+				return false;
+			}
+		}
+	}
+
+	return true;
+}();
 
 } // hpr::mem
 } // hpr
