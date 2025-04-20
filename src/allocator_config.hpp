81bc217d70b56f461c929d8888b46be6f292c54e
esterlein <esterlein.git@gmail.com>
2025-04-20 18:50:36 -0400

    missing fields

--- src/allocator_config.hpp
+++ src/allocator_config.hpp
@@ -2,9 +2,9 @@
 
 #include <array>
 #include <cstdint>
+#include <type_traits>
 
 #include "metapool_proxy.hpp"
-
 
 
 namespace hpr {
@@ -36,34 +36,39 @@ namespace mem {
 		static constexpr auto range_count    = MetapoolRangeArray.size();
 
 		using ProxyArrayType = std::array<MetapoolProxy, range_count>;
+
+		static constexpr uint32_t alignment_quantum {8U};
+
+		static constexpr uint32_t min_stride = range_metadata[0].stride_min;
+		static constexpr uint32_t max_stride = range_metadata[range_count - 1].stride_max;
 	};
 
 
 template <typename T>
 concept IsAllocatorConfig = requires {
 
-	typename T::tag;
-	typename T::ProxyArrayType;
+	typename std::remove_cvref_t<T>::tag;
+	typename std::remove_cvref_t<T>::ProxyArrayType;
 
-	{ T::range_metadata } -> std::same_as<decltype(T::range_metadata)>;
-	{ T::range_count }    -> std::convertible_to<std::size_t>;
+	{ std::remove_cvref_t<T>::range_metadata }    -> std::same_as<decltype(std::remove_cvref_t<T>::range_metadata)>;
+	{ std::remove_cvref_t<T>::range_count }       -> std::convertible_to<std::size_t>;
+	{ std::remove_cvref_t<T>::alignment_quantum } -> std::convertible_to<uint32_t>;
+	{ std::remove_cvref_t<T>::min_stride }        -> std::convertible_to<uint32_t>;
+	{ std::remove_cvref_t<T>::max_stride }        -> std::convertible_to<uint32_t>;
 
-} && []() constexpr {
+} && std::is_same_v<typename std::remove_cvref_t<T>::tag, allocator_config_tag> && []() constexpr {
 
-	const auto& ranges      = T::range_metadata;
-	const std::size_t range_count = ranges.size();
+	const auto& ranges = std::remove_cvref_t<T>::range_metadata;
+	const auto range_count = ranges.size();
 
 	if (range_count == 0) {
 		return false;
 	}
-
 	for (std::size_t i = 0; i < range_count; ++i) {
 		const auto& this_range = ranges[i];
-
 		if (this_range.stride_step == 0) {
 			return false;
 		}
-
 		if ((this_range.stride_step & (this_range.stride_step - 1)) == 0) {
 			uint32_t mask = this_range.stride_step - 1;
 			if ((this_range.stride_min & mask) ||
@@ -79,7 +84,6 @@ concept IsAllocatorConfig = requires {
 				return false;
 			}
 		}
-
 		if (i + 1 < range_count) {
 			const auto& next_range = ranges[i + 1];
 			if (this_range.stride_max + this_range.stride_step
