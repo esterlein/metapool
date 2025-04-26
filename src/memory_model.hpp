fc9b6089ab1c76492661bb9a5a63e811770e0576
esterlein <esterlein.git@gmail.com>
2025-04-26 14:10:36 -0400

    fix

--- src/memory_model.hpp
+++ src/memory_model.hpp
@@ -13,7 +13,7 @@ namespace hpr {
 namespace mem {
 
 
-	static inline constexpr std::size_t arena_size = 1073741824; // 1GB
+	static inline constexpr std::size_t arena_size = 1265162035; // 1.178 GB
 
 
 	using BenchmarkSimpleRegistry =
