80d341e13a649fefa8decb54982fe1332c171f9c
esterlein <esterlein.git@gmail.com>
2025-05-23 16:40:53 -0400

    refactor

---
+++ src/allocator_interface.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+
+namespace mtp {
+
+	template <typename System>
+	struct SystemAllocatorTraits;
+
+} // mtp
+
+
+namespace hpr::mem {
+
+	enum class AllocatorInterface
+	{
+		native,
+		std_adapter,
+		pmr_adapter
+	};
+
+} // hpr::mem
