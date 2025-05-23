fabf9db2987a06cbf627c9937e144e135da41582
esterlein <esterlein.git@gmail.com>
2025-05-23 12:20:13 -0400

    refactor

---
+++ src/mtp_setup.hpp
@@ -0,0 +1,59 @@
+#pragma once
+
+#include "metapool.hpp"
+#include "metapool_config.hpp"
+#include "metapool_registry.hpp"
+
+
+namespace hpr::mem {
+
+
+	static inline constexpr std::size_t arena_size = 2147483648; // 2 GB
+
+
+	enum class AllocatorType
+	{
+		simple,
+		intermediate
+	};
+
+
+	using BenchmarkSimpleRegistry =
+		MetapoolRegistry <
+			Metapool<MetapoolConfig<CapacityFunction::Flat, 2048,  8,     8,  2048>>,
+			Metapool<MetapoolConfig<CapacityFunction::Flat, 2048, 16,  2048,  4128>>,
+			Metapool<MetapoolConfig<CapacityFunction::Flat,    8, 16, 65664, 65680>>
+		>;
+
+	using BenchmarkIntermediateRegistry =
+		MetapoolRegistry <
+			Metapool<MetapoolConfig<CapacityFunction::Flat, 5120, 16,      48,     112>>,
+			Metapool<MetapoolConfig<CapacityFunction::Flat,  512,  8,     112,     136>>,
+			Metapool<MetapoolConfig<CapacityFunction::Flat,  512,  8,    1040,    1048>>,
+			Metapool<MetapoolConfig<CapacityFunction::Flat,  288,  8,   16400,   16408>>,
+			Metapool<MetapoolConfig<CapacityFunction::Flat,   64,  8,   40008,   40016>>,
+			Metapool<MetapoolConfig<CapacityFunction::Flat,   32,  8,  512008,  512016>>,
+			Metapool<MetapoolConfig<CapacityFunction::Flat,   32,  8, 4194560, 4194568>>
+		>;
+
+
+	struct BasicTestsSystem {};
+	struct BenchmarkSimpleSystem {};
+	struct BenchmarkIntermediateSystem {};
+
+	template <> struct SystemAllocatorTraits<BasicTestsSystem>
+	{
+		static constexpr AllocatorType type = AllocatorType::simple;
+	};
+
+	template <> struct SystemAllocatorTraits<BenchmarkSimpleSystem>
+	{
+		static constexpr AllocatorType type = AllocatorType::simple;
+	};
+
+	template <> struct SystemAllocatorTraits<BenchmarkIntermediateSystem>
+	{
+		static constexpr AllocatorType type = AllocatorType::intermediate;
+	};
+
+} // hpr::mem
